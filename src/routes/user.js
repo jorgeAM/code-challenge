3106e38708bfe63979f8bf584f90cc712c858cca
Jorge L <jorge.alfmur@gmail.com>
2020-03-06 19:39:11 -0500

    get user by id endpoint and delete user by id endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,4 +9,38 @@ router.get('/getusers', async (req, res) => {
   res.status(200).json({ users })
 })
 
+router.get('/getusersById/:userId', async (req, res) => {
+  const { userId } = req.params
+
+  try {
+    const user = await User.findById(userId)
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' })
+    }
+
+    res.status(200).json({ user })
+  } catch {
+    res.status(404).json({ message: 'Invalid user id' })
+  }
+})
+
+router.delete('/deleteUsersById/:userId', async (req, res) => {
+  const { userId } = req.params
+
+  try {
+    const user = await User.findById(userId)
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' })
+    }
+
+    await user.remove()
+
+    res.status(200).json({ message: 'ok' })
+  } catch {
+    res.status(404).json({ message: 'Invalid user id' })
+  }
+})
+
 export default router
